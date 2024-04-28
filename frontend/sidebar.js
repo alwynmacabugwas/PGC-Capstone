6b95d750c37961803bd2d05521b1c5b7b003efb6
Raphael De Leon <raphaelaaron.deleon@uap.asia>
2024-04-28 16:10:35 +0800

    fixed css of homepage. did sidebar css not yet finished. will continue later

--- frontend/sidebar.js
+++ frontend/sidebar.js
@@ -1,4 +1,4 @@
-var dropdown = document.getElementsByClassName("dropdown-btn");
+/* var dropdown = document.getElementsByClassName("dropdown-btn");
 var i;
 
 for (i = 0; i < dropdown.length; i++) {
@@ -11,4 +11,25 @@ for (i = 0; i < dropdown.length; i++) {
       dropdownContent.style.display = "block";
     }
   });
-} 
+} */
+
+// Get all dropdown buttons
+var dropdownBtns = document.querySelectorAll('.dropdown-btn');
+
+// Add click event listener to each dropdown button
+dropdownBtns.forEach(function(btn) {
+    btn.addEventListener('click', function() {
+        // Close all dropdowns except the one being clicked
+        dropdownBtns.forEach(function(otherBtn) {
+            if (otherBtn !== btn) {
+                otherBtn.nextElementSibling.style.display = 'none';
+            }
+        });
+        
+        // Toggle current dropdown
+        var dropdownContent = this.nextElementSibling;
+        dropdownContent.style.display === 'block' ? 
+            dropdownContent.style.display = 'none' : 
+            dropdownContent.style.display = 'block';
+    });
+});
