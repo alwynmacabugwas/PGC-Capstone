c6b01f7c2ebe2eb67a3a42326ae10dddc72fe36a
Raphael De Leon <raphaelaaron.deleon@uap.asia>
2024-05-11 23:57:42 +0800

    did the edit button. need to apply the edit function to all pages. will do item config page and apply them to all pages

--- frontend/table_pages/table.js
+++ frontend/table_pages/table.js
@@ -19,6 +19,28 @@ function closeAddItem() {
 document.getElementById("add-item-button").addEventListener("click", openAddItem);
 document.getElementById("discard-add").addEventListener("click", closeAddItem);
 
+function openEditItemPopout() {
+    document.getElementById("overlay").style.display = "block";
+    document.getElementById("edit-item-popout-id").style.display = "block";
+    document.getElementById("confirm-edit").style.display = "block";
+    document.getElementById("discard-edit").style.display = "block";
+    document.querySelector(".third-table").style.display = "block";
+    document.querySelector(".edit-item-popout h1").style.display = "block";
+}
+
+function closeEditItemPopout() {
+    document.getElementById("overlay").style.display = "none";
+    document.getElementById("edit-item-popout-id").style.display = "none";
+    document.getElementById("confirm-edit").style.display = "none";
+    document.getElementById("discard-edit").style.display = "none";
+    document.querySelector(".third-table").style.display = "none";
+    document.querySelector(".edit-item-popout h1").style.display = "none";
+}
+
+document.getElementById("edit-link").addEventListener("click", openEditItemPopout);
+document.getElementById("discard-edit").addEventListener("click", closeEditItemPopout);
+
+
 
 var rowsPerPage = 10; // Adjust as needed
 var currentPage = 1;
